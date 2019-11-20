beb33abac4bc32f7b2b229d0de27f9aa60474c34
Mateusz Dahlke <mdahlke@euvic.pl>
2019-11-20 21:10:15 +0100

    add action script

--- Assets/Scripts/Player/PlayerActionsController.cs
+++ Assets/Scripts/Player/PlayerActionsController.cs
@@ -1,7 +1,57 @@
-namespace PlayerTesting
+using System;
+using UnityEngine;
+
+namespace Player
 {
-    public class PlayerActionsController
+    public class PlayerActionsController : MonoBehaviour
     {
-        
+        public float mainMouseCooldown = 1f;
+        public float additionalMouseCooldown = 5f;
+        public Action MainMouseAction { get; private set; } = () => { };
+        public Action AdditionalMouseAction { get; private set; } = () => { };
+        void Start()
+        {
+            MainMouseAction = () => { Debug.Log("Test!"); };
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Cooldown();
+        }
+
+        public void OnMouseClick()
+        {
+            if (_mainCooldown <= 0f)
+            {
+                MainMouseAction();
+                _mainCooldown = mainMouseCooldown;
+            }
+        }
+
+        public void OnAdditionalMouseClick()
+        {
+            if (_additionalCooldown <= 0f)
+            {
+                AdditionalMouseAction();
+                _additionalCooldown = additionalMouseCooldown;
+            }
+        }
+        private void Cooldown()
+        {
+            if (_mainCooldown > 0f)
+            {
+                _mainCooldown -= Time.deltaTime;
+            }
+
+            if (_additionalCooldown > 0f)
+            {
+                _additionalCooldown -= Time.deltaTime;
+            }
+        }
+
+        private float _mainCooldown = 0f;
+        private float _additionalCooldown = 0f;
+
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,78 +1,107 @@
-﻿using UnityEngine;
- 
-public class PlayerController : MonoBehaviour {
- 
-    public CharacterController characterControler;
-    public GameObject playerCamera;
- 
-    public float movementSpeed = 9.0f;
-    public float strokeHeight = 7.0f;
-    public float actialStrokeHeight = 0f;
- 
-    public float mouseSensitivity = 3.0f;  
-    public float mouseUpDown = 0.0f;
-    public float mouseUpDownRange = 90.0f;
- 
-    void Start () {
-        characterControler = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-     
-    // Update is called once per frame
-    void Update() {
-        Keyboard();
-        Mouse();
-    }
- 
-    private void Keyboard(){
-        PlayerMovement();
-#if UNITY_EDITOR
-        ProgressEditorUtilities();
-#endif
-    }
-
-    private void Mouse(){
-
-        var mouseRightLeft = Input.GetAxis("Mouse X") * mouseSensitivity; 
-        transform.Rotate(0, mouseRightLeft, 0);
-        mouseUpDown -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-         
-        mouseUpDown = Mathf.Clamp(mouseUpDown, -mouseUpDownRange, mouseUpDownRange);
-        playerCamera.transform.localRotation = Quaternion.Euler(mouseUpDown, 0, 0);
-    }
+﻿using System;
+using PlayerTesting;
+using UnityEngine;
 
 
-    private void PlayerMovement()
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
     {
-        var verticalMovement = Input.GetAxis("Vertical") * movementSpeed;
-        var horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed;
-        
-        if(characterControler.isGrounded && Input.GetButton("Jump")){
-            actialStrokeHeight = strokeHeight;
-        } else if (!characterControler.isGrounded ){
-            actialStrokeHeight += Physics.gravity.y * Time.deltaTime;
+
+        public CharacterController characterControler;
+        public GameObject playerCamera;
+
+        public float movementSpeed = 9.0f;
+        public float strokeHeight = 7.0f;
+        public float actialStrokeHeight = 0f;
+
+        public float mouseSensitivity = 3.0f;
+        public float mouseUpDown = 0.0f;
+        public float mouseUpDownRange = 90.0f;
+
+        void Start()
+        {
+            _playerActions = GetComponent(typeof(PlayerActionsController)) as PlayerActionsController;
+            characterControler = GetComponent<CharacterController>();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
-        
-        var move = new Vector3(horizontalMovement, actialStrokeHeight, verticalMovement);
-        move = transform.rotation * move;
-         
-        characterControler.Move(move * Time.deltaTime);
-    }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Keyboard();
+            Mouse();
+        }
+
+        private void Keyboard()
+        {
+            PlayerMovement();
+#if UNITY_EDITOR
+            ProgressEditorUtilities();
+#endif
+        }
+
+        private void Mouse()
+        {
+
+            var mouseRightLeft = Input.GetAxis("Mouse X") * mouseSensitivity;
+            transform.Rotate(0, mouseRightLeft, 0);
+            mouseUpDown -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            mouseUpDown = Mathf.Clamp(mouseUpDown, -mouseUpDownRange, mouseUpDownRange);
+            playerCamera.transform.localRotation = Quaternion.Euler(mouseUpDown, 0, 0);
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                _playerActions.OnMouseClick();
+            }
+
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                _playerActions.OnAdditionalMouseClick();
+            }
+        }
+
+
+        private void PlayerMovement()
+        {
+            var verticalMovement = Input.GetAxis("Vertical") * movementSpeed;
+            var horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed;
+
+            if (characterControler.isGrounded && Input.GetButton("Jump"))
+            {
+                actialStrokeHeight = strokeHeight;
+            }
+            else if (!characterControler.isGrounded)
+            {
+                actialStrokeHeight += Physics.gravity.y * Time.deltaTime;
+            }
+
+            var move = new Vector3(horizontalMovement, actialStrokeHeight, verticalMovement);
+            move = transform.rotation * move;
+
+            characterControler.Move(move * Time.deltaTime);
+        }
 
 #if UNITY_EDITOR
-    private void ProgressEditorUtilities()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        private void ProgressEditorUtilities()
         {
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
 
-        if (Input.GetKeyDown(KeyCode.F3))
-        {
-            Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
-            Cursor.visible = !Cursor.visible;
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                Cursor.lockState = Cursor.lockState == CursorLockMode.Locked
+                    ? CursorLockMode.None
+                    : CursorLockMode.Locked;
+                Cursor.visible = !Cursor.visible;
+            }
         }
-    }
 #endif
+
+        private PlayerActionsController _playerActions;
+    }
 }
