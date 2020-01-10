7b34cb51fd7d041b47642cac718dae11d34a7e3e
Mateusz Dahlke <mdahlke@euvic.pl>
2020-01-10 22:23:56 +0100

    add won state

---
+++ Assets/Scripts/Enemies/EnemiesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemiesController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.childCount == 0)
+        {
+            if (_won)
+            {
+                SceneManager.LoadScene("YouWonScene", LoadSceneMode.Additive);
+                _won = true;
+            }
+        }
+    }
+
+    private bool _won = false;
+
+}
